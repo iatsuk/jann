0be64a56a39099802c4c8a64c1d59b2a19256fa4
a.iatsuk <a.iatsuk@corp.mail.ru>
2020-01-28 23:17:29 +0300

    [visual] fine tune CustomDataset1

--- src/visual/java/net/iatsuk/jaan/visual/datasets/CustomDataset1.java
+++ src/visual/java/net/iatsuk/jaan/visual/datasets/CustomDataset1.java
@@ -13,16 +13,22 @@ public class CustomDataset1 implements Dataset2D {
         for (int i = 0; i < n; i++) {
             int range1 = n / 6;
             int range2 = n / 3;
+            int range3 = n * 2 / 5;
 
             if (i < range1) {
                 // normal1
-                int x = (int) (random.nextGaussian() * maxX * 0.1 + maxX / 4);
-                int y = (int) (random.nextGaussian() * maxY * 0.1 + maxY / 4);
+                int x = (int) (random.nextGaussian() * maxX * 0.07 + maxX / 4);
+                int y = (int) (random.nextGaussian() * maxY * 0.07 + maxY / 4);
                 points[i] = Point2D.of(x, y);
             } else if (i < range1 + range2) {
                 // normal2
                 int x = (int) (random.nextGaussian() * maxX * 0.15 + maxX * 2 / 3);
                 int y = (int) (random.nextGaussian() * maxY * 0.15 + maxY / 3);
+                points[i] = Point2D.of(x, y);
+            } else if (i < range1 + range2 + range3) {
+                // normal3
+                int x = (int) (random.nextGaussian() * maxX * 0.30 + maxX * 3 / 4);
+                int y = (int) (random.nextGaussian() * maxY * 0.30 + maxY * 3 / 4);
                 points[i] = Point2D.of(x, y);
             } else {
                 // uniform
