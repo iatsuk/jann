31066c1eabf7d3e7db37e24173447fa62c9de65b
a.iatsuk <a.iatsuk@corp.mail.ru>
2020-01-29 12:13:09 +0300

    [visual] extract spotify annoy to separate class

--- src/visual/java/net/iatsuk/jaan/visual/DrawDatasets.java
+++ src/visual/java/net/iatsuk/jaan/visual/DrawDatasets.java
@@ -1,13 +1,7 @@
 package net.iatsuk.jaan.visual;
 
-import com.spotify.annoy.jni.base.Annoy;
-import com.spotify.annoy.jni.base.AnnoyIndex;
 import net.iatsuk.jaan.visual.datasets.*;
-import net.iatsuk.jaan.visual.pojo.Point2D;
 import net.iatsuk.jaan.visual.utils.DrawImage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class DrawDatasets {
 
@@ -29,30 +23,7 @@ public class DrawDatasets {
 
         {
             Dataset2D dataset = new CustomDataset1(10000, 1024, 1024, 0xDEADBEEF);
-            Point2D[] points = dataset.points();
-
-            Annoy.Builder builder = Annoy.newIndex(2, Annoy.Metric.EUCLIDEAN);
-            builder.setSeed(0xDEADBEEF);
-            for (int i = 0; i < points.length; i++) {
-                Point2D point = points[i];
-                float x = 1f * point.x;
-                float y = 1f * point.y;
-                System.out.println(i + "\t" + x + "\t" + y);
-                builder.addItem(i, Arrays.asList(x, y));
-            }
-            AnnoyIndex index = builder.build(100);
-
-            points = new Point2D[points.length + 1];
-            System.arraycopy(dataset.points(), 0, points, 0, points.length - 1);
-            Point2D targetPoint = Point2D.of(256, 768, 0xFF0000);
-            points[points.length - 1] = targetPoint;
-            List<Integer> marked = index.getNearestByVector(Arrays.asList(1f*targetPoint.x, 1f*targetPoint.y), 2000);
-            for (int idx: marked) {
-                points[idx].color = 0x00FFFF;
-            }
-            index.close();
-
-            DrawImage.draw("custom1.jpg", 1024, 1024, true, points);
+            DrawImage.draw("custom1.jpg", 1024, 1024, true, dataset.points());
         }
     }
 
---
+++ src/visual/java/net/iatsuk/jaan/visual/DrawDatasetsAnnoy.java
@@ -0,0 +1,72 @@
+package net.iatsuk.jaan.visual;
+
+import com.spotify.annoy.jni.base.Annoy;
+import com.spotify.annoy.jni.base.AnnoyIndex;
+import net.iatsuk.jaan.visual.datasets.*;
+import net.iatsuk.jaan.visual.pojo.Point2D;
+import net.iatsuk.jaan.visual.utils.DrawImage;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class DrawDatasetsAnnoy {
+
+    public static void main(String[] args) {
+        {
+            Dataset2D dataset = new EmptyDataset();
+            DrawImage.draw("emptyAnnoy.jpg", 1024, 1024, true, findNeighbours(dataset.points()));
+        }
+
+        {
+            Dataset2D dataset = new UniformDataset(10000, 1024, 1024, 0xDEADBEEF);
+            DrawImage.draw("uniformAnnoy.jpg", 1024, 1024, true, findNeighbours(dataset.points()));
+        }
+
+        {
+            Dataset2D dataset = new NormalDataset(10000, 1024, 1024, 0xDEADBEEF);
+            DrawImage.draw("normalAnnoy.jpg", 1024, 1024, true, findNeighbours(dataset.points()));
+        }
+
+        {
+            Dataset2D dataset = new CustomDataset1(10000, 1024, 1024, 0xDEADBEEF);
+            DrawImage.draw("custom1Annoy.jpg", 1024, 1024, true, findNeighbours(dataset.points()));
+        }
+    }
+
+    private static Point2D[] findNeighbours(Point2D[] points) {
+        // generate ann index
+        Annoy.Builder builder = Annoy.newIndex(2, Annoy.Metric.ANGULAR);
+        builder.setSeed(0xDEADBEEF);
+        for (int i = 0; i < points.length; i++) {
+            // move the origin into center of image
+            Point2D point = points[i];
+            float x = 1f * point.x - 512;
+            float y = 1f * point.y - 512;
+            builder.addItem(i, Arrays.asList(x, y));
+        }
+        AnnoyIndex index = builder.build(100);
+
+        // add a target point to points
+        Point2D targetPoint = Point2D.of(-510 + 512, -9 + 512, 0xFF0000);
+        points = Arrays.copyOf(points, points.length + 1);
+        points[points.length - 1] = targetPoint;
+
+        // find a nearest neighbours to the target point
+        List<Integer> marked = index.getNearestByVector(Arrays.asList(1f*targetPoint.x - 512, 1f*targetPoint.y - 512), 1000);
+        int color = 0x0FFFFF;
+        for (int i = 0; i < marked.size(); i++) {
+            // fill the nearest neighbours of the target point
+            Point2D point = points[marked.get(i)];
+            point.color = color;
+            if (i % 200 == 0) {
+                color = new Color(color).darker().getRGB();
+            }
+        }
+
+        // close index and return result
+        index.close();
+        return points;
+    }
+
+}
